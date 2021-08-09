a9764744033b6bb2728602982e0864ec8fc54cf9
Vlad Barchenkov <vlad.barchenkov.98@gmail.com>
2021-08-09 12:36:13 +0300

    Initial commit

---
+++ TestQsort/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  TestQsort
+//
+//  Created by Влад Барченков on 09.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configure()
+    }
+}
+
+extension ViewController {
+    private func configure() {
+        view.backgroundColor = #colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)
+    }
+}
+
+
